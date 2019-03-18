181ad8626cc026f4f67847bc9445f18482058497
Christopher Scharun <cscharun@ChristohersMBP2.intern>
2019-03-18 18:08:32 +0100

    Outcommented code in loader for demonstration purposes

--- packages/server/src/loader/fooByUserLoader.ts
+++ packages/server/src/loader/fooByUserLoader.ts
@@ -1,15 +1,19 @@
-import DataLoader = require('dataloader');
-import { getRepository } from 'typeorm';
-import { User } from '../entity/User';
-
-export function communityByUserLoader() {
-  return new DataLoader(async (ids: string[]) => {
-    const usersWithCommunity = await getRepository(User)
-      .createQueryBuilder('user')
-      .leftJoinAndSelect('user.community', 'community')
-      .where('user.id IN (:...ids)', { ids })
-      .getMany();
-
-    return usersWithCommunity.map(user => user.community!);
-  });
-}
+/**
+ * An example how to integrate a custom data loader which works as a join layer inside the database
+ * 
+ * import DataLoader = require('dataloader');
+ * import { getRepository } from 'typeorm';
+ * import { User } from '../entity/User';
+ *
+ * export function fooByUserLoader() {
+ *   return new DataLoader(async (ids: string[]) => {
+ *     const usersWithFoo = await getRepository(User)
+ *       .createQueryBuilder('user')
+ *       .leftJoinAndSelect('user.foo', 'foo')
+ *       .where('user.id IN (:...ids)', { ids })
+ *       .getMany();
+ *
+ *     return usersWithFoo.map(user => user.foo!);
+ *   });
+ * }
+ */
